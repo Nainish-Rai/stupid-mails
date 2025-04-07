bd1ce7cc96c84bfb93f15f01ee7417c1b785166d
Nainish Rai <nainishraisjp@gmail.com>
2025-04-08 02:46:54 +0530

    feat: update loading spinner color for dark mode support

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -146,7 +146,7 @@ export default function HeroSection() {
                         {isLoading ? (
                           <>
                             <svg
-                              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                              className="animate-spin -ml-1 mr-3 h-5 w-5 dark:text-black text-white"
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
                               viewBox="0 0 24 24"
